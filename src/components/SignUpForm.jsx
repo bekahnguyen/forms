8351ed76c4ec210bd716a2d38535d0e8bd900557
bekah <bekahritter@gmail.com>
2024-04-10 20:27:38 -0700

    done for th4 day

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { useState } from "react";
 
+const [userName, setUserName] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState(null);
+
 export default function SignUpForm() {
-  return <h2>Sign Up!</h2>;
+  return (
+    <>
+      <h2>Sign Up!</h2>
+      <form>
+        {" "}
+        <input></input>
+        <input></input>
+        <button type="submit"></button>
+      </form>
+      ;
+    </>
+  );
 }
